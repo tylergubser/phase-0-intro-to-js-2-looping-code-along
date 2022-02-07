7fef1a49c716375e37f8d341aeb892e749cf0a45
tylergubser <tylergubser2200@gmail.com>
2022-02-07 09:15:07 -0600

    commit tree 25a023fefad9b827beeedd0568805cd19c32434a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = [];
+    for(let i=0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(i) {
+    let countDown = i;
+    while (countDown >= 0) {
+  console.log(countDown--)
+}
+}
+l
